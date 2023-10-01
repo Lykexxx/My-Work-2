f8df2ec5817033b583e90492a9e5f64a8c3b4a64
Ilya <lykas1501@mail.ru>
2023-10-01 10:27:43 +0300

    media-burger

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ const burger = document.querySelector('.burger');
+burger.addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('open')
+})
+ 
