8cff2f56086fcd2a38ab369e99d7636804e96e32
Ilya <lykas1501@mail.ru>
2023-10-01 11:00:12 +0300

    fix-4

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ burger.addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('open')
 })
  
+
+const inputForm = document.querySelector('.form__input');
+const form = document.querySelector('.header__form');
+const buttonForm = document.querySelector('.form__btn');
+const span = document.querySelector('.complite');
+buttonForm.addEventListener('click', function() {
+    form.remove();
+    span.classList.remove('none');
+})
